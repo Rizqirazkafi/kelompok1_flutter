95ea19470c4fa82149491410daaefbcf53c6df05
Rizqi Black <90085130+RizqiBlack19@users.noreply.github.com>
2022-11-21 22:42:10 +0700

    Part 7 done

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -2,8 +2,10 @@
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:kelompok1_flutter/app/contents/color.dart';
 import 'package:lottie/lottie.dart';
 import '../../../routes/app_pages.dart';
+
 import '../controllers/introduction_controller.dart';
 
 class IntroductionView extends GetView<IntroductionController> {
@@ -12,42 +14,61 @@ class IntroductionView extends GetView<IntroductionController> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          "Al-Quran Apps",
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        SizedBox(height: 20),
-        Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 50,
-          ),
-          child: Text(
-            "Sesibuk itukah kamu hingga sampai lupa membaca Al-Quran",
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Al-Quran Apps",
             style: TextStyle(
-              fontSize: 16,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
             ),
-            textAlign: TextAlign.center,
           ),
-        ),
-        SizedBox(height: 10),
-        // ignore: sized_box_for_whitespace
-        Container(
-          width: 250,
-          height: 250,
-          child: Lottie.asset("assets/lottie/animasi-quran.json"),
-        ),
-        SizedBox(height: 30),
-        ElevatedButton(
-          onPressed: () => Get.offAllNamed(Routes.HOME),
-          child: Text("Mulai"),
-        ),
-      ],
-    )));
+          SizedBox(height: 40),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 50,
+            ),
+            child: Text(
+              "Sesibuk itukah kamu hingga sampai lupa membaca Al-Quran",
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          SizedBox(height: 70),
+          // ignore: sized_box_for_whitespace
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Container(
+              width: 325,
+              height: 325,
+              child: Lottie.asset("assets/lottie/animasi-quran.json"),
+            ),
+          ),
+          SizedBox(height: 40),
+          ElevatedButton(
+            onPressed: () => Get.offAllNamed(Routes.HOME),
+            child: Text(
+              "Mulai",
+              style: TextStyle(
+                color: Get.isDarkMode ? appPurpleDark : appWhite,
+              ),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Get.isDarkMode ? appWhite : appPurple,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              padding: EdgeInsets.symmetric(
+                horizontal: 125,
+                vertical: 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:kelompok1_flutter/app/contents/color.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: appLight,
       debugShowCheckedModeBanner: false,
       title: "Application",
       initialRoute: Routes.INTRODUCTION,
